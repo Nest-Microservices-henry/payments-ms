0ce250c150b334875e36953bc8f00adc2edd201f
Henry Merchan <henry.merchan@halopowered.com>
2024-07-03 19:31:41 -0300

    payment integration

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { envs } from 'src/config';
+import { NATS_SERVICE, envs } from 'src/config';
 import { Stripe } from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentService');
 
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ){}
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
     const { currency, items, orderId } = paymentSessionDto;
 
@@ -27,7 +32,7 @@ export class PaymentsService {
       //add the order ID
       payment_intent_data: {
         metadata: {
-            orderId: orderId
+          orderId: orderId,
         },
       },
 
@@ -37,39 +42,48 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
   }
 
-  async stripeWebhook( req: Request, res: Response) {
+  async stripeWebhook(req: Request, res: Response) {
     const sig = req.headers['stripe-signature'];
-   
 
     let event: Stripe.Event;
     //const endpointSecret = "whsec_714d648225adde1fa197d00747a9b6d24346187e68189b6116cc765c84ce591d";
     const endpointSecret = envs.stripeEndpointSecret;
 
-    try{
-        event = this.stripe.webhooks.constructEvent(req['rawBody'], sig, endpointSecret);
-
-    }catch(err){
-        res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
     }
 
-    switch(event.type) {
-        case 'charge.succeeded':
-            const chargeSucceeded = event.data.object
-            console.log({
-                metadata: chargeSucceeded.metadata
-            });
-            break;
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.orderId,
+          receiptUrl: chargeSucceeded.receipt_url,
+        }
 
-            default:
-                console.log(`event ${event.type} not handled`)
+        //this.logger.log({payload})
+        this.client.emit('payment.succeeded', payload);
+        break;
+
+      default:
+        console.log(`event ${event.type} not handled`);
     }
 
-    return res.status(200).json({sig})
-
+    return res.status(200).json({ sig });
   }
 }
